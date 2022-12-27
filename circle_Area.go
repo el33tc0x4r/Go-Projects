32caf0d105716773f2f0c520f87949190ca50a4f
Yiğit Gürbulak <78663763+yigofyork@users.noreply.github.com>
2022-12-27 18:47:18 +0300

    Add files via upload

---
+++ circle_Area.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	Pi := 3.14
+	var radius, Area float64
+	fmt.Println("Enter radius: ")
+	fmt.Scanln(&radius)
+
+	Area = (Pi) * (radius * radius)
+	fmt.Println("Circle Area: ", Area)
+}
