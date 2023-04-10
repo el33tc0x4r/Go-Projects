15af0ceca94755d0c5f6adae2299654c9454280d
Yiğit Gürbulak <78663763+yigofyork@users.noreply.github.com>
2023-04-10 23:01:58 +0300

    Update atmProject.go

--- atmProject.go
+++ atmProject.go
@@ -3,7 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	hesap := 10000
+	var bakiye int
+	fmt.Println("Bakiye Girin: ")
+	fmt.Scanln(&bakiye)
+
 	for true {
 		fmt.Println(`**********İŞLEMLER**********
 		1. Para Çekme 
@@ -22,10 +25,10 @@ func main() {
 			var cekilecek_tutar int
 			fmt.Scanln(&cekilecek_tutar)
 
-			if cekilecek_tutar <= hesap {
+			if cekilecek_tutar <= bakiye {
 				fmt.Println("Paranızı Kutudan Alınız")
-				hesap -= cekilecek_tutar
-				fmt.Println("Bakiyeniz: ", hesap)
+				bakiye -= cekilecek_tutar
+				fmt.Println("Bakiyeniz: ", bakiye)
 			} else {
 				fmt.Println("Yetersiz Bakiye")
 			}
@@ -34,10 +37,10 @@ func main() {
 
 			var yatırılacak_tutar int
 			fmt.Scanln(&yatırılacak_tutar)
-			hesap += yatırılacak_tutar
-			fmt.Println("Yeni Bakiye: ", hesap)
+			bakiye += yatırılacak_tutar
+			fmt.Println("Yeni Bakiye: ", bakiye)
 		} else if islem == 3 {
-			fmt.Println("Hesap Bakiyeniz: ", hesap)
+			fmt.Println("Hesap Bakiyeniz: ", bakiye)
 		} else if islem == 4 {
 			break
 		}
