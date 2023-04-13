6e7c5081758ca084ffc5509bea08c702d353f8ed
Yiğit Gürbulak <78663763+yigofyork@users.noreply.github.com>
2023-04-13 04:12:05 +0300

    Add files via upload

---
+++ blockchain-3.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"strings"
+)
+
+type BitcoinBlock struct {
+	previous_block_hash string
+	transaction_list    []string
+	block_data          string
+	block_hash          string
+}
+
+func NewBitcoinBlock(previous_block_hash string, transaction_list []string) *BitcoinBlock {
+	block_data := strings.Join(transaction_list, "-") + " - " + previous_block_hash
+	block_hash := fmt.Sprintf("%x", sha256.Sum256([]byte(block_data)))
+	return &BitcoinBlock{previous_block_hash, transaction_list, block_data, block_hash}
+}
+
+func main() {
+
+	fmt.Println("Name: ")
+	var x string
+	fmt.Scanln(&x)
+
+	fmt.Println("Who do you want to send it to: ")
+	var y string
+	fmt.Scanln(&y)
+
+	b1 := " sends 10 BTC to "
+
+	block1 := NewBitcoinBlock("firstblock", []string{x, b1, y})
+	fmt.Printf("Block 1 data: %s\n", block1.block_data)
+	fmt.Printf("Block 1 hash: %s\n", block1.block_hash)
+}
