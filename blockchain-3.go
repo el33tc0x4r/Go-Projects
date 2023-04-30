eaf6116abf32972110b02a19689cb9abe4962180
Radeonares32 <56337258+Radeonares32@users.noreply.github.com>
2023-04-30 23:41:54 +0300

    Update blockchain-3.go

--- blockchain-3.go
+++ blockchain-3.go
@@ -6,17 +6,17 @@ import (
 	"strings"
 )
 
-type BitcoinBlock struct {
+type BlockChainBlock struct {
 	previous_block_hash string
 	transaction_list    []string
 	block_data          string
 	block_hash          string
 }
 
-func NewBitcoinBlock(previous_block_hash string, transaction_list []string) *BitcoinBlock {
+func NewBlockChainBlock(previous_block_hash string, transaction_list []string) *BlockChainBlock {
 	block_data := strings.Join(transaction_list, "-") + " - " + previous_block_hash
 	block_hash := fmt.Sprintf("%x", sha256.Sum256([]byte(block_data)))
-	return &BitcoinBlock{previous_block_hash, transaction_list, block_data, block_hash}
+	return &BlockChainBlock{previous_block_hash, transaction_list, block_data, block_hash}
 }
 
 func main() {
@@ -31,7 +31,7 @@ func main() {
 
 	b1 := " sends 10 BTC to "
 
-	block1 := NewBitcoinBlock("firstblock", []string{x, b1, y})
+	block1 := NewBlockChainBlock("firstblock", []string{x, b1, y})
 	fmt.Printf("Block 1 data: %s\n", block1.block_data)
 	fmt.Printf("Block 1 hash: %s\n", block1.block_hash)
 }
